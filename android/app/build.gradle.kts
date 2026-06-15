9eee3ed4d9636f76ffeee37ad380c7bae8a5a804
Sneha <sneha@centralfolio.local>
2026-06-15 15:42:57 -0400

    ci: upgrade Retrofit and converter-kotlinx-serialization to 2.11.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     
     // Networking & Serialization
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     
